163729818b8beef7de088b4b8f48e05014bb833d
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2025-05-19 17:03:51 -0400

    feat(nextjs): configure assetPrefix using phase for local and production builds

--- projects/nextjs/next.config.ts
+++ projects/nextjs/next.config.ts
@@ -1,9 +1,17 @@
-const nextConfig = {
-  distDir: 'dist',
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+import type { NextConfig } from 'next';
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
+
+const nextConfig = (phase: string): NextConfig => {
+  const isLocal = phase === PHASE_DEVELOPMENT_SERVER;
+
+  return {
+    distDir: 'dist',
+    output: 'export',
+    assetPrefix: isLocal ? '' : '/nextjs/',
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
 };
 
 export default nextConfig;
