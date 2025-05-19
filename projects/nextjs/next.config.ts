7eabb479cade29da8260648a72501bb95c414959
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2025-05-19 17:20:17 -0400

    feat(asset): try using env var to prefix
    Signed-off-by: Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>

--- projects/nextjs/next.config.ts
+++ projects/nextjs/next.config.ts
@@ -3,12 +3,15 @@ import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
 
 const nextConfig = (phase: string): NextConfig => {
   const isLocal = phase === PHASE_DEVELOPMENT_SERVER;
+  const assetPrefix = isLocal ? '' : '/nextjs/';
 
   return {
     distDir: 'dist',
     output: 'export',
-    assetPrefix: isLocal ? '' : '/nextjs/',
-    basePath: isLocal ? '' : '/nextjs',
+    assetPrefix,
+    env: {
+      NEXT_PUBLIC_ASSET_PREFIX: assetPrefix,
+    },
     eslint: {
       ignoreDuringBuilds: true,
     },
